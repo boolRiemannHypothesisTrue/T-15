eb106760122c775a1736716e13da669cb492841d
Mark <boolRiemannHypothesisTrue@hotmail.com>
2025-09-18 14:30:28 +0300

    upd

--- analysis.m
+++ analysis.m
@@ -1,15 +1,44 @@
 %% 
-addpath(genpath('C:\Users\MSI\Desktop\Курчатовский институт\Т-10 Анализ рядов')) % вспомогательные функции и все такое
+clear all 
+clc
+addpath(genpath('C:\Users\MSI\Documents\GitHub\T-15')) % вспомогательные функции и все такое
+addpath(genpath("C:\Users\MSI\Documents\GitHub\T-10")) % для мультифрактального спектра, тоже дополнительные функции
+cd('C:\Users\MSI\Documents\GitHub\T-15')
 %%
+    load("loaded_data.mat")
 
-data = ReadData("T15MD_4452.mat",0.5,200/2.7);
-s1 = data(:,1);
-dt = 1e-3;
-t = (1:length(s1)) * dt;
-t = t';
-plot(t,s1)
+if ~exist("s1","var")
 
-%%
+    data = ReadData("T15MD_4452.mat",0.5,200/2.7);
+    s1 = data(:,1);
+    dt = 1e-3;
+    t = (1:length(s1)) * dt;
+    t = t';
+
+    plot(t,s1,'k')
+
+    
+    tt = title('$Signal$');
+    
+    set(gca, 'FontSize', 16, 'LineWidth', 2)
+    set(gcf, 'Color', 'white')
+    
+    
+    labels = [tt,...
+              xlabel('$Time,\  ms$', 'FontSize', 14), ...
+              ylabel('$Ion\ saturation\ current,\ A$', 'FontSize', 14)];
+    set(labels, 'Interpreter', 'latex');
+
+
+end
+
+if ~exist("loaded_data.mat","file")
+    
+    save loaded_data
+
+end
+
+% интервал где есть плазма, определяется визуально
 
 idx = find(t > 0.26 & t < 1.7);
 
@@ -21,7 +50,7 @@ t_cleaned = t(idx);
 t = t_cleaned;
 y = s1_cleaned(:);
 
-wname = 'db8';
+wname = 'db6';
 level = floor(log2(length(y))) - 1;
 w = modwt(y, wname, level);
 
@@ -68,167 +97,132 @@ labels = [tt,hh,...
           xlabel('$Time,\ \mu s$', 'FontSize', 14), ...
           ylabel('$Ion\ saturation\ current,\ A$', 'FontSize', 14)];
 set(labels, 'Interpreter', 'latex');
-clear t wname level energy energy_ratio y w w_trend trend trend_levels
+clear  wname level energy energy_ratio y w w_trend trend trend_levels
 
 
-%%  Histogram
+%% PDF
 
-fp = s1_cleaned;
-figure
-bins = 100; % increase for better skewness/kurtosis visualization
+fp = y_detrended;
 
-histogram(fp, bins); % вместо histfit используем histogram
+% Данные (нормированные)
+x = (fp - mean(fp)) / std(fp);  
 
-ylabel('$Number\ of\ observations$', Interpreter='latex')
-xlabel('$Floating\ potential$', Interpreter='latex')
-legend('Floating potential')
-title("$ Floating\ potential\ histogram $", Interpreter="latex")
-set(gca, 'FontSize', 16, 'LineWidth', 2) 
-set(gcf, 'Color', 'white')
+skew = skewness(x); % коэффициент ассиметрии
+kurt = kurtosis(x); % коэффициент эксцесса 
+
+% Оценка плотности с помощью ядра
+[f_pdf, xi] = ksdensity(x,NumPoints=length(x)/2);
 
 
-%% Skewness/ Kurtosis
-
-skew = skewness(fp); % коэффициент ассиметрии
-kurt = kurtosis(fp); % коэффициент эксцесса 
-
-%% Fourier spectrum
+figure; hold on;
 
 
-warning off
-s = fp';
-Ts = 1e-6;
-Fs = 1 / Ts;
-n = length(s);
+plot(xi, f_pdf, 'o', 'MarkerSize', 8, 'Color', 'k');
 
-% Расчёт спектра
-y = fft(s);
-fshift = (-n/2:n/2-1)*(Fs/n);
-yshift = fftshift(y);
-z = abs(yshift);
+% Гауссиан
+x_gauss = linspace(min(xi), max(xi), 500);
+y_gauss = (1/sqrt(2*pi)) * exp(-0.5 * x_gauss.^2);
+plot(x_gauss, y_gauss, 'k', 'LineWidth', 2);
 
-% Положительные частоты
-positive_idx = fshift > 0;
-f_pos = fshift(positive_idx);
-
-%  f^{-1}
-x_ref = linspace(1e5, 1e7, 200);   % частотный диапазон
-C = 1e8;                           % масштаб
-y_ref = C ./ x_ref;                % f^{-1}
-
-% График
-figure
-loglog(fshift, z,'k')
-hold on
-loglog(x_ref, y_ref, 'k', 'LineWidth', 1.5)
-labels = [title('$Fourier\ spectrum\ of\ signal$', 'FontSize', 14), ...
-          xlabel('$f, Hz$', 'FontSize', 14), ...
-          ylabel('$s(f), rel.units$', 'FontSize', 14)];
-          
-
-% Подпись над линией f^{-1}
-xtxt = x_ref(round(end/6));           % середина линии по X
-ytxt = y_ref(round(end/2)) *15;     % чуть выше линии по Y
-text(xtxt, ytxt, '$s(f) \sim f^{-1}$', ...
-     'FontSize', 14, 'Color', 'k', 'Interpreter', 'latex')
-set(labels, 'Interpreter', 'latex');
-set(gca, 'FontSize', 16, 'LineWidth', 2);
-set(gcf, 'Color', 'white');
-
-savefig(fullfile("Fourier_spectrum_decline"))
-
-%% Mask for wavelet transform 
+xlabel('$Normalized\ signal\ values $', 'Interpreter','latex');
+ylabel('$Probability\ density$', 'Interpreter','latex');
 
 
-% % Выбираем для анализа интервал по времени 
+
+
+titlestr = ['$PDF\ of\ signal\ vs\ Gaussian(skew = ' num2str(skew,2) ', kurt = ' num2str(kurt,2) ')$'];
+title(titlestr, 'Interpreter','latex');
+
+
+set(gca,'FontSize',16,'LineWidth',2);
+set(gcf,'Color','white');
+legend({'$Experimental\ PDF$ ','$Standard\ Gaussian$'}, 'Interpreter','latex');
+grid on
+box on
+
+
+
+
+%% Fourier Spectrum
 % 
-% time_min = 300; % dt не более 5
-% time_max = 305;
-% % Создаем маску для индексов
-% mask = (time >= time_min) & (time <= time_max);
-% % Извлекаем данные
-% time_for_wavelet = time(mask);
-% fp_for_wavelet= fp(mask);
-
-
-
-
-% %%%%%%%%%%%%%%%%%%%%%%%%%% UNCOMMENT FOR ABS. LVL WAVELET TRANSFORMS
-% %% Wavelet MHAT. ABS.LVL
-% %  Scale 30
-% % 
-% figure
+% Fs = 1/dt;                % частота дискретизации
+% N = length(s1);           % число точек
+% FP_fft = fft(s1);         % прямое преобразование Фурье
+% FP_mag = abs(FP_fft)/N;   % амплитуда (нормируем)
+% f = (0:N-1)*(Fs/N);       % частоты от 0 до Fs
 % 
-% tic
+% % --- основной спектр ---
+% figure;
 % subplot(2,1,1)
-%  cwt(fp_for_wavelet,1:0.01:30, 'mexh','abslvl',[100 1000]);
-% title("MHAT, scale 30")
-% toc
+% loglog(f(1:floor(N/2)), 2*FP_mag(1:floor(N/2)), 'k', 'LineWidth', 1.5); 
+% set(gca,'XScale','log','YScale','log','FontSize',16,'LineWidth',2); 
+% set(gcf,'Color','white');
+% labels = [title('$Fourier\ spectrum\ of\ signal\ (log-log\ scale)$', 'FontSize', 14), ...
+%           xlabel('$f, Hz$', 'FontSize', 14), ...
+%           ylabel('$s(f), rel.units$', 'FontSize', 14)];
+% set(labels, 'Interpreter', 'latex');
+% %grid on;
 % 
-% %  Scale 100
+% % --- данные для половины спектра ---
+% f_half = f(1:floor(N/2));
+% s_half = 2*FP_mag(1:floor(N/2));
+% 
+% lf = log10(f_half);
+% ls = log10(s_half);
+% 
+% % --- фигура с кусочными фитами ---
+% 
+% % --- интервалы для фита (задаёт пользователь) ---
+% intervals = [1e-2 1e0;  
+%              1e0 1e2;  
+%              1e2 1e3];  
+% 
 % subplot(2,1,2)
-% tic
+% hold on;
+% colors = lines(size(intervals,1));
+% legtxt = cell(size(intervals,1),1);
 % 
-%  cwt(fp_for_wavelet,1:0.1:100, 'mexh','abslvl',[100 1000]);
-% title("MHAT, scale 100")
-% toc
+% for k = 1:size(intervals,1)
+%     fmin = intervals(k,1);
+%     fmax = intervals(k,2);
 % 
+%     idx = (f_half >= fmin) & (f_half <= fmax);
 % 
-% %% Wavelet Morlet ABS.LVL
-% figure
-% tic
-% subplot(2,1,1)
-%  cwt(fp_for_wavelet,1:0.01:30, 'morl','abslvl',[100 1000]);
+%     % Линейная аппроксимация в log-log
+%     p = polyfit(lf(idx), ls(idx), 1);
+%     alpha = p(1); % наклон
 % 
-% toc
-% % Wavelet Morlet. Scale 100
+%     % Вычисляем аппроксимацию
+%     f_fit = logspace(log10(fmin), log10(fmax), 200);
+%     s_fit = 10.^(polyval(p, log10(f_fit)));
 % 
-% tic
-% subplot(2,1,2)
-%  cwt(fp_for_wavelet,1:0.1:100, 'morl','abslvl',[100 1000]);
-%  toc
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%% END UNCOMMENT
+%     % Рисуем log-log линию
+%     loglog(f_fit, s_fit, 'Color', colors(k,:), 'LineWidth', 2);
+% 
+%     % Подпись для легенды
+%     legtxt{k} = sprintf('$%.1f\\ \\leq f(Hz) \\leq %.1f$,  $\\alpha=%.2f$', fmin, fmax, abs(alpha));
+% end
+% 
+% set(gca,'XScale','log','YScale','log','FontSize',16,'LineWidth',2); 
+% set(gcf,'Color','white');
+% labels = [title('$Piecewise\ linear\ fit\ of\ spectrum$', 'FontSize', 14), ...
+%           xlabel('$f, Hz$', 'FontSize', 14), ...
+%           ylabel('$s(f), rel.units$', 'FontSize', 14)];
+% set(labels, 'Interpreter', 'latex');
+% 
+% legend(legtxt, 'Interpreter','latex','FontSize',12,'Location','best');
+% %grid on;
+% 
+% % в конце кода
+% subplot(2,1,1);
+% ax1 = gca;
+% subplot(2,1,2);
+% box on;
+% ax2 = gca;
+% 
+% linkaxes([ax1 ax2], 'x');  % синхронизируем ось X
+% linkaxes([ax1 ax2], 'y');  % синхронизируем ось X
 
-%% Wavelet transform -> Skeleton
-
-figure 
-fp_for_wavelet = s1_cleaned;
-t = t_cleaned; % временная ось
-
-% Вейвлет и шкала
-wname = 'morl'; % например Морле
-scales2 = 1:0.01:100;
-cwt_coeffs2 = cwt(fp_for_wavelet, scales2, wname);
-
-% Берем модуль
-cfs_abs = abs(cwt_coeffs2);
-
-% Матрица скелета
-skeleton = zeros(size(cfs_abs));
-
-for k = 1:size(cfs_abs,2)   % по времени
-    % Максимумы
-    [~,locsMax] = findpeaks(cfs_abs(:,k));
-    skeleton(locsMax,k) = 1; 
-    
-    % Минимумы
-    [~,locsMin] = findpeaks(-cfs_abs(:,k));
-    skeleton(locsMin,k) = -1; 
-end
-
-% Отображение скелетограммы
-imagesc(t, scales2, skeleton);
-axis xy;
-colormap([0 0 1; 1 1 1; 1 0 0]); % синий=минимумы, белый=фон, красный=максимумы
-set(gca, 'YScale', 'log', 'YTick', 2.^(0:10));
-ylabel('Scale, μs');
-xlabel('Time, μs ');
-title(['Skeleton of Wavelet (' wname ') coefficients']);
-
-set(gca, 'FontSize', 16, 'LineWidth', 2)
-set(gcf, 'Color', 'white')
-
-%savefig(fullfile(results_path,['Wavelet ' num2str(wname)]))
 
 %% 
 dim = 3;
@@ -236,7 +230,11 @@ dim = 3;
 Np = 300;
 correlationDimension(s1_cleaned,lag,dim,"NumPoints",Np);
 %%
-ts = s1_cleaned;
-[~,elag,edim] = phaseSpaceReconstruction(ts)
+[~,elag,edim] = phaseSpaceReconstruction(fp);
 
 %%
+phaseSpaceReconstruction(fp,elag,edim)
+
+%%
+Np = 300;
+correlationDimension(s1_cleaned,lag,dim,"NumPoints",Np);
